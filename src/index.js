01a7460beafebb748dd6596fba99ba17638deb1d
krispthan <44071872+krispthan@users.noreply.github.com>
2019-02-08 21:50:06 -0800

    worked on the first prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const userObj={
+    name: 'Christine',
+    luckyNumber:luckyNumber()
+}
+
+function luckyNumber(){
+    return Math.floor(Math.random() * 1000) + 1;
+}
+
+function greeting(userObj){
+    return(
+     <div className="container">
+        <h1>Greeting {userObj.name}</h1>
+        <h2 className = "text-muted">Your lucky Number is {userObj.luckyNumber}</h2>
+    </div>
+    )
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    // <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
